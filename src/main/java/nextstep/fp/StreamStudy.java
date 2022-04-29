cc8ddeebca68b515e5befc5f1f5ba7dd38e3e134
moonstal1506 <moonstal1506@gmail.com>
2022-04-29 19:49:01 +0900

    람다,스트림, optional실습

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,12 +23,23 @@ public class StreamStudy {
         return count;
     }
 
+    //    단어의 길이가 12자를 초과하는 단어를 추출한다.
+//            12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 추출한다.
+//            sorted() method 활용
+//    단어 중복을 허용하지 않는다. 즉, 서로 다른 단어 100개를 추출해야 한다.
+//            distinct() method 활용
+//    추출한 100개의 단어를 출력한다. 모든 단어는 소문자로 출력해야 한다.
+//            String.toLowerCase() method 활용
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream().filter(w->w.length()>12)
+                .sorted(Comparator.comparing(String::length).reversed())
+                .distinct()
+                .limit(100)
+                .forEach(w-> System.out.println(w.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +51,8 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number -> number > 3)
+                .map(number->number*2)
+                .reduce(0,(x,y)->x+y);
     }
 }
