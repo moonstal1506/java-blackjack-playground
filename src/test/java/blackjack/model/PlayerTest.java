3cba8d83a5e9b4653e2eb4511b6ed89cc909b6ce
moonstal1506 <moonstal1506@gmail.com>
2022-06-03 02:02:51 +0900

    fix: 테스트코드 추상클래스 구체클래스로 변경

--- src/test/java/blackjack/model/PlayerTest.java
+++ src/test/java/blackjack/model/PlayerTest.java
@@ -11,7 +11,7 @@ class PlayerTest {
     @DisplayName("플레이어는 카드를 받는다")
     @Test
     void receiveCard(){
-        Player player = new Player();
+        Player player = new Dealer();
         player.receiveCard(new Card(Denomination.THREE,Suit.CLUBS));
         player.receiveCard(new Card(Denomination.TWO,Suit.CLUBS));
         Assertions.assertThat(player.getCards().retain()).isEqualTo("3클로버, 2클로버");
