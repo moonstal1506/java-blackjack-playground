b7c25f7cd89f4dfef0acc97e5b135011b7dffa48
moonstal1506 <moonstal1506@gmail.com>
2022-06-02 19:06:17 +0900

    feat: 카드 생성

---
+++ src/main/java/blackjack/Main.java
@@ -0,0 +1,9 @@
+package blackjack;
+
+import blackjack.controller.BlackjackController;
+
+public class Main {
+    public static void main(String[] args) {
+        new BlackjackController().run();
+    }
+}
